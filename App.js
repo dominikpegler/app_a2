24f89fda9f3581e7c95e623cf80f3dd16ff868de
donkndetphone <dinkopeg@posteo.net>
2021-05-02 00:35:59 +0200

    commit saturday

--- App.js
+++ App.js
@@ -1,29 +1,34 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, View } from 'react-native';
-import { Text } from 'react-native-elements';
+import 'react-native-gesture-handler';
+import HomeScreen from './screens/HomeScreen';
+import AlbumScreen from './screens/AlbumScreen';
+import ArtistScreen from './screens/ArtistScreen';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text h4> App mit 3 Komponenten</Text>
-      <Text>1. Startseite mit Suchfeld für Artist und Anzeige Favourites</Text>
-      <Text>2. API-Abfrage der Alben des Artist</Text>
-      <Text>3. Seite mit den Metadaten des Albums (Titel, Bild, Year, Genre und Description)</Text>
-      <Text>4. Möglichkeit Artist zu Favourits hinzuzufügen</Text>
-      <StatusBar style="auto" />
+const Stack = createStackNavigator();
 
-      <View style={{position: 'absolute', left: "1em", right: 0, bottom: "1em"}}><Text>Dominik PEGLER a01468373</Text></View>
-    </View>
-
-  );
+class App extends React.Component {
+  render() {
+    return (
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen
+            name="Home"
+            component={HomeScreen}
+          />
+          <Stack.Screen
+            name="Artist"
+            component={ArtistScreen}
+            />
+          <Stack.Screen
+            name="Album"
+            component={AlbumScreen}
+          />
+        </Stack.Navigator>
+      </NavigationContainer>
+    );
+  }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#c9c9c9',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
