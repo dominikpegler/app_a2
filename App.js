a5bbdceb3079b873d180a55fa9462fb7fb8586f8
donkndetphone <dinkopeg@posteo.net>
2021-05-01 11:12:47 +0200

    edit

--- App.js
+++ App.js
@@ -1,13 +1,21 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
+import { Text } from 'react-native-elements';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text h4> App mit 3 Komponenten</Text>
+      <Text>1. Startseite mit Suchfeld für Artist und Anzeige Favourites</Text>
+      <Text>2. API-Abfrage der Alben des Artist</Text>
+      <Text>3. Seite mit den Metadaten des Albums (Titel, Bild, Year, Genre und Description)</Text>
+      <Text>4. Möglichkeit Artist zu Favourits hinzuzufügen</Text>
       <StatusBar style="auto" />
+
+      <View style={{position: 'absolute', left: "1em", right: 0, bottom: "1em"}}><Text>Dominik PEGLER a01468373</Text></View>
     </View>
+
   );
 }
 
